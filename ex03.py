a19e63a31ec675fad41eb9b91689c103b16665f0
Manuela de Caria Bertanha <102591519+ManuelaBertanha@users.noreply.github.com>
2022-08-25 15:50:29 -0300

    Update ex03.py

--- ex03.py
+++ ex03.py
@@ -1,4 +1,8 @@
 # Exercício 3:
+'''Faça um programa que gere cartas de baralho aleatoriamente (valor e naipe). Para isso, crie uma função que sorteie um valor entre 1 e 13,
+sendo: 1 = Às, 11 = J, 12 = Q e 13 = K. Para os naipes, faça o sorteio de um valor entre 1 e 4, sendo: 1 = PAUS, 2 = ESPADAS, 3 = OUROS e 4 = COPAS.
+A função deverá retornar uma combinação correspondente à uma carta sorteada. Para um determinado jogo, são necessárias 5 cartas para um jogador.
+Crie uma lista com 5 cartas aleatórias e exiba essas cartas. Lembre-se de que não é possível ter duas cartas iguais, já que estamos usando apenas um baralho.'''
 
 import random
 
